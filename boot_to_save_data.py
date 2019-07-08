e4b595c94fab5deccc346a5ce1333936e8ea3a23
Dr. Shields <31747379+DoctorShields@users.noreply.github.com>
2019-07-08 15:46:36 -0400

    simpler version of boot.py for saving data

--- boot_to_save_data.py
+++ boot_to_save_data.py
@@ -1,11 +1,6 @@
-import board
-import digitalio
 import storage
-
-switch = digitalio.DigitalInOut(board.D7) # this is the switch 
-switch.direction = digitalio.Direction.INPUT
-switch.pull = digitalio.Pull.UP
+from adafruit_circuitplayground.express import cpx
 
 # switch to the left means your computer can write to the CPX
 # switch to the right means CircuitPython can save data to the CPX
-storage.remount("/", switch.value)
+storage.remount("/", cpx.switch)
